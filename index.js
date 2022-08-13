9dc92da1f7386386970ffaddd1adaa800b5b0194
Cstoscup <109521820+Cstoscup@users.noreply.github.com>
2022-08-13 18:44:03 -0400

    Added current weather icons and updated current time API

--- index.js
+++ index.js
@@ -2,56 +2,22 @@ function formatDate(response) {
     let long = response.data.coord.lon;
     let lat = response.data.coord.lat;
     let timestamp = response.data.dt;
-    let apiKey = "AIzaSyAmEhwxiu-sfYbG1VkFzx-T3wXsGIDqK-8";
-    let url = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat}%2C${long}&timestamp=${timestamp}&key=AIzaSyAmEhwxiu-sfYbG1VkFzx-T3wXsGIDqK-8`;
-    axios.get(url).then(getDate);
-}
-
-function getDate(response) {
-    let timezone = response.data.timeZoneId;
-    let url = `https://worldtimeapi.org/api/timezone/${timezone}`;
+    let apiKey = "6aa24761b999470a92c6399feefd0f88";
+    let url = `https://api.ipgeolocation.io/timezone?apiKey=${apiKey}&lat=${lat}&long=${long}`;
     axios.get(url).then(displayDate);
 }
 
 function displayDate(response) {
-    let days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-    ];
-    let day = days[response.data.day_of_week];
-    let hours = fixHours(parseInt(response.data.datetime.slice(11, 13)));
-    let minutes = response.data.datetime.slice(14, 16);
-    let unit = getTimeUnit(parseInt(response.data.datetime.slice(11, 13)));
-    let time = `${hours}:${minutes} ${unit}`;
-
-    document.querySelector("#current-date-time").innerHTML = day + "<br>" + time;
-}
-
-function fixHours(hours) {
-  if (hours > 12) {
-    hours -= 12;
-  }
-  return hours;
-}
-
-function fixMinutes(minutes) {
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-  return minutes;
-}
-
-function getTimeUnit(hours) {
-  if (hours >= 12) {
-    return "PM";
-  } else {
-    return "AM";
-  }
+    let date = response.data.date_time_txt;
+    let date_array = date.split(",");
+    let time = response.data.time_12;
+    let time_array = time.split("");
+    time_array.splice(5, 3);
+    if (time_array[0] === "0") {
+        time_array.shift();
+    }
+    time = time_array.join("");
+    document.querySelector("#current-date-time").innerHTML = date_array[0] + "<br>" + time;
 }
 
 function clickMe(event) {
@@ -76,6 +42,7 @@ function displayGeo(response) {
     document.querySelector("#weather").innerHTML = response.data.weather[0].description;
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind").innerHTML = response.data.wind.speed;
+    formatDate(response);
 }
 
 function displaySearch(response) {
@@ -85,6 +52,7 @@ function displaySearch(response) {
     document.querySelector("#weather").innerHTML = response.data.weather[0].description;
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind").innerHTML = response.data.wind.speed;
+    document.querySelector("#current-weather-icon").setAttribute("src", `images/${response.data.weather[0].icon}.svg`)
     formatDate(response);
 }
 
